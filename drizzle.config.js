72621e78445bb79979599ee480fe100ad7f99143
Swayamanand11 <118674900+Swayamanand11@users.noreply.github.com>
2025-09-04 23:42:34 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default{
     schema: "./utils/schema.js" ,
     dialect: 'postgresql',
     dbCredentials: {
-        url:'postgresql://neondb_owner:npg_zLQHSCP36WkF@ep-damp-dream-ad127qga-pooler.c-2.us-east-1.aws.neon.tech/AI-Mock?sslmode=require&channel_binding=require',
+        url:'',
     }
 };
